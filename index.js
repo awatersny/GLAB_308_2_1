a5062a524c7148d20df20416fbffb6f9101018de
awatersny <awatersny@gmail.com>
2025-03-21 15:28:11 -0400

    Give the adventurer "Robin" a roll method

--- index.js
+++ index.js
@@ -11,5 +11,13 @@ const adventurer = {
       type: "Flea",
       inventory: ["small hat", "sunglasses"]
     }
+  },
+  roll (mod = 0) {
+    const result = Math.floor(Math.random() * 20) + 1 + mod;
+    console.log(`${this.name} rolled a ${result}.`)
   }
+}
+
+for(i = 0; i < 20; i++) {
+  adventurer.roll(0)
 }
